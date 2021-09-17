eb21b6e49dcfcd2006f54d4b8832bc06f88a276f
muhammed sinan.p <psinan32@gmail.com>
2021-09-17 09:48:56 +0530

    Add platform

--- Rope.js
+++ Rope.js
@@ -4,7 +4,7 @@ class Rope {
       bodyA: bodyA,
       pointB: pointB,
       stiffness: 1.2,
-      length: 450,
+      length: 500,
     };
 
     this.pointB = pointB;
@@ -20,7 +20,7 @@ class Rope {
 
       //set the value of stroke to hide the rope
       stroke(48, 22, 8);
-      strokeWeight(4);
+      strokeWeight(0);
 
       line(pointB.x, pointB.y, pointA.x, pointA.y);
 
--- sketch.js
+++ sketch.js
@@ -90,7 +90,7 @@ function draw() {
 
 function mouseDragged() {
   //set the position of hero when mouse is dragged
-  Matter.Body.setPosition(hero.body, { x: 600, y: 765});
+  Matter.Body.setPosition(hero.body, { x: mouseX, y: mouseY});
 }
 
 
